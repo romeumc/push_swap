7684edb7e93f598226ad8a71b89a10ceeed13a87
rmartins <rmartins@student.42lisboa.com>
2021-05-14 19:56:11 +0100

    check sorted

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rmartins <rmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 17:22:08 by rmartins          #+#    #+#             */
-/*   Updated: 2021/05/14 17:59:17 by rmartins         ###   ########.fr       */
+/*   Updated: 2021/05/14 18:04:58 by rmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 # include "ft_ansi.h"
 
 t_list	*load_list(int argc, char **argv);
-void	free_list(t_list *elem);
-void	print_list(t_list *list);
+// void	free_list(t_list *elem);
+// void	print_list(t_list *list);
 int		validate_args(int argc, char **argv);
 void	display_error(char *title, char *description);
 void	check_arg_error(int valid_list);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rmartins <rmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 16:09:12 by rmartins          #+#    #+#             */
-/*   Updated: 2021/05/14 18:00:23 by rmartins         ###   ########.fr       */
+/*   Updated: 2021/05/14 18:16:17 by rmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,28 @@ void	print_stack(int *stack, int size)
 	int	i;
 
 	i = 0;
+	size--;
 	while (i < size)
 	{
 		printf ("arg[%d]:%d\n", i, stack[i]);
 		i++;
 	}
+}
+
+int	check_sorted(int *stack, int size)
+{
+	int	i;
+
+	i = 0;
+	size--;
+	while (i < size - 1 && stack[i] > stack[i + 1])
+	{
+		i++;
+	}
+	if (i == size - 1)
+		return (EXIT_SUCCESS);
+	else
+		return (EXIT_FAILURE);
 }
 
 int	main(int argc, char **argv)
@@ -59,12 +76,13 @@ int	main(int argc, char **argv)
 		valid_list = validate_args(argc, argv);
 		check_arg_error(valid_list);
 		stack_a = load_stack(argc, argv);
-		print_stack(stack_a, argc - 1);
 		//list = load_list(argc, argv);
 	}
 	// printf("list size:%d\n", ft_lstsize(list));
 	//print_list(list);
 	//free_list(list);
+	print_stack(stack_a, argc);
+	printf("sorted:%d", check_sorted(stack_a, argc));
 	(void)stack_b;
 	free(stack_a);
 }
